ef881e34029542556400cb52dd9b6cc4c2a1368a
Javier D'Alessandro <javierosvaldodalessandro@gmail.com>
2023-10-18 15:25:50 -0300

    Mejoras

--- Ingreso_Carne/Clases/Class1.vb
+++ Ingreso_Carne/Clases/Class1.vb
@@ -48,5 +48,34 @@ order by id desc "
     Public Sub vaciar(ByVal Dgv As DataGridView)
         Dgv.Rows.Clear()
     End Sub
+    Public Function ValiCodBarra(Cod_barra)
+        SQL = "SELECT cod_barra FROM Ingreso_carne WHERE fecha = '" & hoy & "' "
+        libSql.Consulta(SQL, resul2, transa, resultado, mensaje)
+
+        If resul2.Rows.Count > 0 Then
+            For Each row As DataRow In resul2.Rows
+                If row.Item("cod_barra").ToString() = Cod_barra.ToString() Then
+                    MessageBox.Show("No puede repetir codigo de barra   '" & Cod_barra & "' ya fue ingresado")
+                    Return False
+                End If
+            Next
+        End If
+
+
+        Return True
+    End Function
+
+
+    Public Function Valiingre(ByVal Cod_barra As String, ByVal kilos As String) As Boolean
+        If kilos = 0 Or Cod_barra = 0 Then
+            MessageBox.Show("Debe ingresar Codigo de Barra válido y Kilos válidos")
+            Return False
+        Else
+            Return True
+        End If
+    End Function
+
+
+
 
 End Class
--- Ingreso_Carne/Formularios/Form1.vb
+++ Ingreso_Carne/Formularios/Form1.vb
@@ -11,14 +11,28 @@ Public Class Form1
     Private RptReporteIngresos As Object
 
     Public Sub btnGrabar_Click(sender As Object, e As EventArgs) Handles btnGrabar.Click
-        Dim Cod_barra As String = txtCodBarra.Text
-        Dim kilos As Double = txtKilos.Text
-        val1.Grabar(Cod_barra, kilos)
-        val1.vaciar(DataGridView1)
-        val1.Vertodos(DataGridView1)
-        txtCodBarra.Text = ""
-        txtKilos.Text = ""
-        txtCodBarra.Focus()
+        Dim Cod_barra As String = Val(txtCodBarra.Text)
+        Dim kilos As Integer = Val(txtKilos.Text)
+
+        If val1.Valiingre(Cod_barra, kilos) Then
+
+            If val1.ValiCodBarra(Cod_barra) Then
+
+
+
+                val1.Grabar(Cod_barra, kilos)
+            val1.vaciar(DataGridView1)
+            val1.Vertodos(DataGridView1)
+            txtCodBarra.Text = ""
+            txtKilos.Text = ""
+            txtCodBarra.Focus()
+
+        End If
+
+
+        End If
+
+
     End Sub
 
 
@@ -81,5 +95,19 @@ Public Class Form1
 
         frmReporteingreso.MostrarReporte(verFecha)
         frmReporteingreso.Show()
+    End Sub
+
+    Private Sub txtKilos_TextChanged(sender As Object, e As EventArgs) Handles txtKilos.TextChanged
+
+    End Sub
+
+    Private Sub txtKilos_DragDrop(sender As Object, e As DragEventArgs) Handles txtKilos.DragDrop
+
+    End Sub
+
+    Private Sub txtKilos_KeyDown(sender As Object, e As KeyEventArgs) Handles txtKilos.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            btnGrabar_Click(sender, e)
+        End If
     End Sub
 End Class
